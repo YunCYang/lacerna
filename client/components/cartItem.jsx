7c59140b0bc2fd1d5d41bd3ad4b8f3a390493351
YunCYang <ericeorl2013@gmail.com>
2020-02-11 20:58:15 -0800

    user can now update product quantity in cart using the input field

--- client/components/cartItem.jsx
+++ client/components/cartItem.jsx
@@ -12,7 +12,7 @@ const CartItem = props => {
   React.useEffect(
     () => {
       setQuantity(props.item.quantity);
-    }
+    }, []
   );
 
   return (
@@ -54,7 +54,116 @@ const CartItem = props => {
           <div className='qt'>
             <div className='qt-input'>
               <input type="text" value={quantity} onChange={
-                e => setQuantity(e.target.value)
+                e => {
+                  const test = /^[1-9]\d*$/;
+                  if (test.exec(e.target.value)) {
+                    setQuantity(e.target.value);
+                  }
+                }
+              } onBlur={
+                () => {
+                  let init = {};
+                  if (quantity > props.item.quantity) {
+                    if (userId) {
+                      init = {
+                        method: 'POST',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                          login: 'login',
+                          productId: props.item.detail.productId,
+                          size: props.item.detail.size,
+                          userId: userId
+                        })
+                      };
+                    } else {
+                      init = {
+                        method: 'POST',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                          login: 'nologin',
+                          productId: props.item.detail.productId,
+                          size: props.item.detail.size
+                        })
+                      };
+                    }
+                    const request = [];
+                    const paCopy = [...productArray];
+                    for (let i = 0; i < quantity - props.item.quantity; i++) {
+                      request.push('/api/cart/product');
+                      paCopy.push({ ...props.item.detail });
+                    }
+                    Promise.all(
+                      request.map(item => {
+                        return fetch(item, init)
+                          .then(res => res.json())
+                          .then(res => false);
+                      })
+                    ).then(
+                      () => {
+                        dispatch({
+                          type: STOCK,
+                          payload: paCopy
+                        });
+                      }
+                    );
+                  } else if (quantity < props.item.quantity) {
+                    if (userId) {
+                      init = {
+                        method: 'DELETE',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                          login: 'login',
+                          productId: props.item.detail.productId,
+                          size: props.item.detail.size,
+                          userId: userId
+                        })
+                      };
+                    } else {
+                      init = {
+                        method: 'DELETE',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                          login: 'nologin',
+                          productId: props.item.detail.productId,
+                          size: props.item.detail.size
+                        })
+                      };
+                    }
+                    const request = [];
+                    const paCopy = [...productArray];
+                    for (let i = 0; i < props.item.quantity - quantity; i++) {
+                      request.push('/api/cart/product');
+                      for (let i = 0; i < paCopy.length; i++) {
+                        if (paCopy[i].productId === props.item.detail.productId &&
+                          paCopy[i].size === props.item.detail.size) {
+                          paCopy.splice(i, 1);
+                          break;
+                        }
+                      }
+                    }
+                    Promise.all(
+                      request.map(item => {
+                        return fetch(item, init)
+                          .then(res => false);
+                      })
+                    ).then(
+                      () => {
+                        dispatch({
+                          type: STOCK,
+                          payload: paCopy
+                        });
+                      }
+                    );
+                  }
+                }
               }/>
             </div>
             <div className='qt-button'>
