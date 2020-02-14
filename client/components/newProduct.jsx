2a6cc96ef11fc4d572c14937a44b7850d9fbb6a6
YunCYang <ericeorl2013@gmail.com>
2020-02-14 11:56:44 -0800

    added new product section

--- client/components/newProduct.jsx
+++ client/components/newProduct.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import ProductCard from './productCard';
 
 const NewProduct = () => {
+  const [productList, setProductList] = React.useState([]);
+
+  React.useEffect(
+    () => {
+      fetch('/api/product/new')
+        .then(res => res.json())
+        .then(res => setProductList(res));
+    }, []
+  );
+
+  const createProductCard = () => {
+    if (productList.length) {
+      return productList.map((item, index) => <ProductCard product={item} key={`new${index}`} />);
+    } else return <h3>Loading...</h3>;
+  };
+
   return (
-    <div className="new-product"></div>
+    <div className="new-product">
+      <div className='title'>
+        <div className="wrapper">
+          <span>What&apos;s New</span>
+        </div>
+      </div>
+      <div className='new-content'>
+        <div className="wrapper">
+          {createProductCard()}
+        </div>
+      </div>
+    </div>
   );
 };
 
